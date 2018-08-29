acf692d8609eceae200753deba72d713f18650d9
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2018-08-29 19:52:54 +0000

    fixing some bugs in the install script

--- pre_build/install_build.sh
+++ pre_build/install_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+sudo mkdir -p  /usr/local/include/opencv2/face/
 sudo install face.hpp /usr/local/include/opencv2/
 cd headers
 sudo install *.hpp /usr/local/include/opencv2/face/
 cd ../lib
-sudo install *.so /usr/local/lib/
+sudo install opencv_face.so /usr/local/lib/
+sudo install opencv_face.so.3.4 /usr/local/lib/
+sudo install opencv_face.so.3.4.1 /usr/local/lib/
 cd ..
 
  
